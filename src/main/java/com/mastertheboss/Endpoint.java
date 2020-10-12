f18f7c86c65f6e3e9d950a5cff0ef26f7bd8c6b0
Tim Lukashevich <efreet@tut.by>
2020-10-12 18:14:49 +0300

    Turned on verbose logging for wildfly created second info endpoint for unprivileged role "user" Added unprivileged role "user" into web.xml added beans.xml (Dont know if I need it)

--- src/main/java/com/mastertheboss/Endpoint.java
+++ src/main/java/com/mastertheboss/Endpoint.java
@@ -1,17 +1,21 @@
 package com.mastertheboss;
 
-import org.json.JSONObject;
-import org.wildfly.security.auth.server.SecurityDomain;
-import org.wildfly.security.auth.server.SecurityIdentity;
-import org.wildfly.security.authz.Attributes;
-
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
+
+import org.json.JSONObject;
+import org.wildfly.security.auth.server.SecurityDomain;
+import org.wildfly.security.auth.server.SecurityIdentity;
+import org.wildfly.security.authz.Attributes;
 
 @PermitAll
 @Path("/")
@@ -21,18 +25,25 @@ public class Endpoint {
     private SecurityContext securityContext;
 
     @GET
-    @Path("/info")
+    @Path("/infoAdmin")
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed({"admin"})
-    public String getInfo() {
-        return identityInfoAsJson();
+    public String infoAdmin() {
+        return "Only admin: " +  identityInfoAsJson();
+    }
+
+    @GET
+    @Path("/infoUser")
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({"user"})
+    public String infoUser() {
+    	return "Only user: " +  identityInfoAsJson();
     }
 
     @POST
     @RolesAllowed({"admin"})
     @Consumes(MediaType.APPLICATION_JSON)
     public Response saveData(Data data) {
-
         System.out.println(data);
         return Response.ok("Done!").build();
     }
