0625a2e64b9eb7c6391da22e43804df9058e92c3
Tim Lukashevich <efreet@tut.by>
2020-10-13 13:56:41 +0300

    Добавлен модуль resteasy-ejb-FORM

---
+++ resteasy-ejb-FORM/src/main/java/com/mastertheboss/Endpoint.java
@@ -0,0 +1,106 @@
+package com.mastertheboss;
+
+import java.security.Principal;
+
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+import org.json.JSONObject;
+import org.wildfly.security.auth.server.SecurityDomain;
+import org.wildfly.security.auth.server.SecurityIdentity;
+import org.wildfly.security.authz.Attributes;
+
+@Stateless
+@PermitAll
+@Path("/")
+public class Endpoint {
+
+	@Context
+	private SecurityContext securityContext;
+
+	@GET
+	@Path("/infoPublic")
+	@Produces(MediaType.APPLICATION_JSON)
+	@PermitAll
+	public String infoPublic() {
+		return "endpoint for all: " + identityInfoAsJsonPublic();
+	}
+
+	@GET
+	@Path("/infoAdmin")
+	@Produces(MediaType.APPLICATION_JSON)
+	@RolesAllowed({ "admin" })
+	public String infoAdmin() {
+		return "Only admin: " + identityInfoAsJson();
+	}
+
+	@GET
+	@Path("/infoUser")
+	@Produces(MediaType.APPLICATION_JSON)
+	@RolesAllowed({ "user" })
+	public String infoUser() {
+		return "Only user: " + identityInfoAsJson();
+	}
+
+	@POST
+	@RolesAllowed({ "admin" })
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response saveData(Data data) {
+		System.out.println(data);
+		return Response.ok("Done!").build();
+	}
+
+	private String identityInfoAsJson() {
+		SecurityIdentity identity = SecurityDomain.getCurrent().getCurrentSecurityIdentity();
+		JSONObject response = new JSONObject();
+		response.put("name", securityContext.getUserPrincipal().getName());
+		response.put("attributes", attributesAsJson(identity.getAttributes()));
+		return response.toString();
+	}
+
+	private String identityInfoAsJsonPublic() {
+		JSONObject response = new JSONObject();
+		
+		SecurityDomain secDomain = SecurityDomain.getCurrent();
+		response.put("SecurityDomain", secDomain);
+		if (secDomain == null) {
+			return response.toString();
+		}
+		
+		SecurityIdentity identity = secDomain.getCurrentSecurityIdentity();
+		response.put("SecurityIdentity", identity);
+		if (identity == null) {
+			return response.toString();
+		} else {
+			response.put("attributes", attributesAsJson(identity.getAttributes()));
+		}
+		Principal userPrincipal = securityContext.getUserPrincipal();
+		response.put("userPrincipal", userPrincipal);
+		if (userPrincipal == null) {
+			return response.toString();
+		} else {
+			response.put("userPrincipal.name", userPrincipal.getName());
+		}
+
+		return response.toString();
+	}
+
+	private JSONObject attributesAsJson(Attributes attributes) {
+		JSONObject attributesObject = new JSONObject();
+		for (String attribute : attributes.keySet()) {
+			attributesObject.put(attribute, attributes.get(attribute));
+		}
+		return attributesObject;
+	}
+
+}
---
+++ resteasy-ejb-FORM/src/main/java/com/mastertheboss/JAXActivator.java
@@ -0,0 +1,9 @@
+package com.mastertheboss;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class JAXActivator extends Application {
+	// Left empty intentionally
+}
